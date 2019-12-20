4ced83f6324e18db4eacf81c159bde88f0aa459a
Rahul Kallil <rahulkallil2@gmail.com>
2019-12-20 23:37:58 +0530

    services

--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def test(request):
+    # print("HIIIIIII")
+    # if request.method == 'POST':
+    #     print("HIIIIIII")
+    #     postdata = request.post.copy()
+    #     print("HIIIIIII")
     return render(request,'frontend/index.html')
 
 
