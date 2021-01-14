4c4fcb3b2e20a3bfe2623a4fc28cb0133b296f59
Fedrick Nishant <fedrick@yodaplus.com>
2021-01-15 01:31:14 +0530

    conatct backend completed

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import re
+import phonenumbers
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
+
+
+def validate_phonenumber(phone):
+    """
+    Validate if input is a phone number
+    """
+    try:
+        formatted_phone = phonenumbers.parse(phone)
+        assert phonenumbers.is_valid_number(formatted_phone)
+    except (phonenumbers.NumberParseException, AssertionError):
+        raise ValidationError(
+            'Enter a valid phone number (e.g. +12125552368).')
+
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField("Full Name",max_length=1024)
+    email = models.EmailField("Email")
+    phone = models.CharField("Phone",
+        validators=[validate_phonenumber], max_length=17)
+    message = models.TextField("Message")
+
+    def __str__(self):
+        return self.name
